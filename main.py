f4cfb0bfb76cd9994fbbedc5163eacb6b38b2014
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2025-07-29 22:17:21 +0900

    Implement dockerized environment of app.

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import nfc
-
-def main():
-    try:
-        clf = nfc.ContactlessFrontend('usb')
-        print("NFCリーダーが見つかりました")
-        clf.connect(rdwr={'on-connect': lambda tag: print("Tag!")})
-    except Exception as e:
-        print(f"NFCリーダーが見つかりません（スキップ）: {e}")
-        while True:
-            pass
-
-if __name__ == "__main__":
-    main()
+print("起動中...")
+with nfc.ContactlessFrontend('usb') as clf:
+    clf.connect(rdwr={'on-connect': lambda tag: print("Tag:", tag)})
